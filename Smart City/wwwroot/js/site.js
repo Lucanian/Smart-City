e8c4d650e5f8fc122d96973283a1a3e88af6080b
Lucian Cimpeanu <cimpeanu.lucian@yahoo.com>
2017-04-08 17:42:08 +0300

    Add project files.

---
+++ Smart City/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿// Write your Javascript code.
+
+(function () {
+
+var $sideBarAndWrapper = $("#SideBar,#BaraSus");
+var $icon = $("#SidebarToggle i.fa");
+
+
+$("#SidebarToggle").click(function () {
+    $sideBarAndWrapper.toggleClass("hide-sidebar");
+
+    if ($sideBarAndWrapper.hasClass("hide-sidebar")) {
+        $icon.removeClass("fa-angle-left");
+        $icon.addClass("fa-angle-right");
+    }
+
+    else {
+        $icon.addClass("fa-angle-left");
+        $icon.removeClass("fa-angle-right");
+    }
+
+
+});
+
+})();
